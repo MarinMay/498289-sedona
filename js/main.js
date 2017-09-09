caad00cab4f615d496dc6613e6fe17083663c660
MarinMay <vaganova.m.a@gmail.com>
2017-09-09 22:35:24 +0500

    доьбавлен js

---
+++ js/main.js
@@ -0,0 +1,45 @@
+﻿var button = document.querySelector(".hotel-search-button");
+var hotel = document.querySelector(".search-form");
+var form = document.querySelector(".form-hotel-search");
+var arrivalDate = document.querySelector("[name=arrival-date]");
+var dateOfDeparture = document.querySelector("[name=date-of-departure]");
+var adults = document.querySelector("[name=number-of-adults]");
+var child = document.querySelector("[name=number-of-child]");
+var storageArrivalDate = localStorage.getItem("arrivalDate");
+var storageDateOfDeparture = localStorage.getItem("dateOfDeparture");
+var storageAdults = localStorage.getItem("adults");
+var storageChild = localStorage.getItem("child");
+
+hotel.classList.add("search-form-close");
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  hotel.classList.toggle("search-form-close");
+  arrivalDate.focus();
+  if (storageArrivalDate || storageDateOfDeparture || storageAdults || storageChild) {
+    arrivalDate.value = storageArrivalDate;
+    dateOfDeparture.value = storageDateOfDeparture;
+    adults.value = storageAdults;
+    child.value = storageChild;
+  };
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate && !dateOfDeparture && !adults && !child) {
+  evt.preventDefault();
+  } 
+  else {
+  localStorage.setItem("arrivalDate", arrivalDate.value);
+  localStorage.setItem("dateOfDeparture", dateOfDeparture.value);
+  localStorage.setItem("adults", adults.value);
+  localStorage.setItem("child", child.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (!hotel.classList.contains("search-form-close")) {
+        hotel.classList.add("search-form-close");
+      }
+    }
+  });
